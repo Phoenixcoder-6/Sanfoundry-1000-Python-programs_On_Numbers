e8ccc1485b7d5e2d8329e54ae2077f8821735fff
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2023-06-18 13:09:02 +0530

    Prime number in a given range.py

--- Prime number in a given range.py
+++ Prime number in a given range.py
@@ -1,1 +1,16 @@
+# First, we will take the input:  
+lower_value = int(input ("Please, Enter the Lowest Range Value: "))  
+upper_value = int(input ("Please, Enter the Upper Range Value: "))  
+  
+print("The prime numbers are:")
+
+for numbers in range(lower_value, upper_value+1):
+    if numbers >1:
+        for i in range(2, numbers):
+            if (numbers % i == 0):
+                break
+        else:
+            print(numbers)
+
+
 
